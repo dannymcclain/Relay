4bfdf18ff3aa63be2c204c856b0107ea0f2d3a19
Danny <dannyxmcclain@gmail.com>
2018-07-27 12:23:48 -0500

    got the search to work

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 function constructHistory(historyItems) {
-  console.log(historyItems);
+  var i;
+  for (i = 0; i < historyItems.length; i++) {
+    console.log(historyItems[i].url);
+  }
 }
 
 chrome.history.search({
       text: '',
-      maxResults: 30
+      maxResults: 5
     }, constructHistory);
