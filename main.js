118b62d52b583bac9783807c506dd825e335af77
Danny <dannyxmcclain@gmail.com>
2018-07-27 13:56:24 -0500

    got devices, cant get urls

--- main.js
+++ main.js
@@ -1,11 +1,41 @@
-function constructHistory(historyItems) {
-  var i;
-  for (i = 0; i < historyItems.length; i++) {
-    console.log(historyItems[i].url);
-  }
+// const historyList = document.getElementById('history-list');
+//
+// function constructHistory(historyItems) {
+//   var i;
+//   for (i = 0; i < historyItems.length; i++) {
+//     var historyItem = document.createElement('li');
+//     var historyLink = document.createElement('a');
+//     historyLink.href = historyItems[i].url;
+//     historyLink.innerHTML = historyItems[i].title;
+//     historyItem.appendChild(historyLink);
+//     historyList.appendChild(historyItem);
+//     // console.log(historyItems[i]);
+//   }
+// }
+//
+// chrome.history.search({
+//       text: ''
+//     }, constructHistory);
+
+function showDevices(theDevices) {
+      var sessionList = document.getElementById('session-list');
+      var i;
+      for (i = 0; i < theDevices.length; i++) {
+        var device = document.createElement('li');
+        device.innerHTML = theDevices[i].deviceName + ' ' + theDevices[i].sessions[0].window.tabs[0].url;
+        sessionList.appendChild(device);
+        // console.log(theDevices[i].sessions[i].window.tabs);
+        console.log(theDevices[i]);
+      }
+    }
+chrome.sessions.getDevices({},showDevices);
+
+function showTab(theTabs) {
+    var i = 0;
+    for (i = 0; i < theTabs.length; i++) {
+      console.log(theTabs[i].tab.Tab.url)
+    }
+    // console.log(theTabs);
 }
 
-chrome.history.search({
-      text: '',
-      maxResults: 5
-    }, constructHistory);
+chrome.tabs.query({}, showTab);
