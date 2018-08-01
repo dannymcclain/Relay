81de841f040b62645a1bac1f5b818dde2c023b4b
Danny <dannyxmcclain@gmail.com>
2018-08-01 10:13:48 -0500

    removed everything related to closing tabs

--- main.js
+++ main.js
@@ -16,14 +16,12 @@ function getTabsFromSessions(sessions) {
 }
 
 function generateHtml(data) {
-  console.log(data);
   var html = ``;
   for (let i = 0; i < data.length; i++) {
     html += `<h2>${data[i].name}</h2>`
     html += `<ul>`
     for (let j = 0; j < data[i].tabs.length; j++) {
-      html += `<li><a href="${data[i].tabs[j].url}">${data[i].tabs[j].title}</a><a id="${data[i].tabs[j].sessionId}" href="#" class="close">Close</a></li>`;
-
+      html += `<li><a href="${data[i].tabs[j].url}">${data[i].tabs[j].title}</a></li>`;
     }
     html += `</ul>`;
   }
@@ -35,25 +33,5 @@ function renderHtmlToDom(data) {
     const html = generateHtml(tabsByDevice);
     const container = document.getElementsByClassName('container')[0];
     container.innerHTML = html;
-
-      console.log("Cool face");
-    const closeTabButtonNodes = document.querySelectorAll('.close');
-    console.log(closeTabButtonNodes);
-    const closeTabButtonElements = Array.from(closeTabButtonNodes);
-    closeTabButtonElements.map((closeTabButton)=>{
-      closeTabButton.addEventListener('click', function() {
-          // chrome.tabs.remove(closeTabButton.id);
-          console.log(closeTabButton.id);
-          console.log("The button should close something");
-        })
-    })
-
 }
-
-const closeTabButton = document.getElementById('close-tab');
-closeTabButton.onclick = function closeTab() {
-  chrome.tabs.remove([session_syncXILfG+GxjSJ1QJblT3Yqew==.10]);
-  console.log('Did we remove it?');
-}
-
 chrome.sessions.getDevices({}, renderHtmlToDom);
