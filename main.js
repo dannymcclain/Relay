2b63bbe72df856b243a2994341b4f526f0e7ced9
Danny <dannyxmcclain@gmail.com>
2018-08-01 10:00:15 -0500

    tried to close remote tabs, failed

--- main.js
+++ main.js
@@ -16,24 +16,44 @@ function getTabsFromSessions(sessions) {
 }
 
 function generateHtml(data) {
-  // console.log(tabsByDevice);
+  console.log(data);
   var html = ``;
   for (let i = 0; i < data.length; i++) {
     html += `<h2>${data[i].name}</h2>`
     html += `<ul>`
     for (let j = 0; j < data[i].tabs.length; j++) {
-      html += `<li><a href="${data[i].tabs[j].url}">${data[i].tabs[j].title}</a></li>`
+      html += `<li><a href="${data[i].tabs[j].url}">${data[i].tabs[j].title}</a><a id="${data[i].tabs[j].sessionId}" href="#" class="close">Close</a></li>`;
+
     }
-    html += `</ul>`
+    html += `</ul>`;
   }
   return html;
 }
 
 function renderHtmlToDom(data) {
-  const tabsByDevice = getTabsByDevice(data);
-  const html = generateHtml(tabsByDevice);
-  const container = document.getElementsByClassName('container')[0];
-  container.innerHTML = html;
+    const tabsByDevice = getTabsByDevice(data);
+    const html = generateHtml(tabsByDevice);
+    const container = document.getElementsByClassName('container')[0];
+    container.innerHTML = html;
+
+      console.log("Cool face");
+    const closeTabButtonNodes = document.querySelectorAll('.close');
+    console.log(closeTabButtonNodes);
+    const closeTabButtonElements = Array.from(closeTabButtonNodes);
+    closeTabButtonElements.map((closeTabButton)=>{
+      closeTabButton.addEventListener('click', function() {
+          // chrome.tabs.remove(closeTabButton.id);
+          console.log(closeTabButton.id);
+          console.log("The button should close something");
+        })
+    })
+
+}
+
+const closeTabButton = document.getElementById('close-tab');
+closeTabButton.onclick = function closeTab() {
+  chrome.tabs.remove([session_syncXILfG+GxjSJ1QJblT3Yqew==.10]);
+  console.log('Did we remove it?');
 }
 
 chrome.sessions.getDevices({}, renderHtmlToDom);
